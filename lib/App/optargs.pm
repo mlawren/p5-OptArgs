bd7c80920d1200201f15ea4b4280c0062bd44f21
Mark Lawrence <nomad@null.net>
2012-08-04 11:58:58 +1200

    Version bump

--- lib/App/optargs.pm
+++ lib/App/optargs.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use OptArgs;
 use lib 'lib';
-our $VERSION = '0.0.3';
+our $VERSION = '0.0.4';
 
 $OptArgs::COLOUR = 1;
 
@@ -84,7 +84,7 @@ App::optargs - implementation of the optargs(1) command
 
 =head1 VERSION
 
-0.0.3 Development release.
+0.0.4 Development release.
 
 =head1 SYNOPSIS
 
