0793a22ba75c38263449eba04b06c4d91cf627c3
Mark Lawrence <nomad@null.net>
2012-08-04 11:55:49 +1200

    Release version 0.0.3

--- lib/App/optargs.pm
+++ lib/App/optargs.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use OptArgs;
 use lib 'lib';
-our $VERSION = '0.0.2';
+our $VERSION = '0.0.3';
 
 $OptArgs::COLOUR = 1;
 
@@ -84,7 +84,7 @@ App::optargs - implementation of the optargs(1) command
 
 =head1 VERSION
 
-0.0.2 Development release.
+0.0.3 Development release.
 
 =head1 SYNOPSIS
 
