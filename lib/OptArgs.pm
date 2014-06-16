cd6ed55efa2336ee77945234221ecdd9730ffc01
Mark Lawrence <nomad@null.net>
2014-06-16 16:42:29 +0200

    Set version 0.1.14

--- lib/OptArgs.pm
+++ lib/OptArgs.pm
@@ -9,7 +9,7 @@ use Exporter::Tidy
 use Getopt::Long qw/GetOptionsFromArray/;
 use List::Util qw/max/;
 
-our $VERSION = '0.1.13_1';
+our $VERSION = '0.1.14';
 our $COLOUR  = 0;
 our $ABBREV  = 0;
 our $SORT    = 0;
@@ -662,7 +662,7 @@ OptArgs - integrated argument and option processing
 
 =head1 VERSION
 
-0.1.13_1 (yyyy-mm-dd)
+0.1.14 (yyyy-mm-dd)
 
 =head1 SYNOPSIS
 
