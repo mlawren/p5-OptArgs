66e52659587eaed8bd03f5514f1abf46f9c352e1
Mark Lawrence <nomad@null.net>
2016-04-11 15:15:38 +0200

    Start development on version 0.1.21_1

--- lib/App/optargs.pm
+++ lib/App/optargs.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use OptArgs;
 use lib 'lib';
-our $VERSION = '0.1.20';
+our $VERSION = '0.1.21_1';
 
 $OptArgs::COLOUR = 1;
 
@@ -84,7 +84,7 @@ App::optargs - implementation of the optargs(1) command
 
 =head1 VERSION
 
-0.1.20 development release.
+0.1.21_1 development release.
 
 =head1 SYNOPSIS
 
