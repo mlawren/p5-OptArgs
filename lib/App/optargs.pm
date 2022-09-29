47023e17b8c46247413e37bb226693016294560d
Mark Lawrence <nomad@null.net>
2022-09-29 10:53:59 +0200

    Set version to 0.1.21

--- lib/App/optargs.pm
+++ lib/App/optargs.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use OptArgs;
 use lib 'lib';
-our $VERSION = '0.1.21_1';
+our $VERSION = '0.1.21';
 
 $OptArgs::COLOUR = 1;
 
--- lib/OptArgs.pm
+++ lib/OptArgs.pm
@@ -9,7 +9,7 @@ use Exporter::Tidy
 use Getopt::Long qw/GetOptionsFromArray/;
 use List::Util qw/max/;
 
-our $VERSION       = '0.1.21_1';
+our $VERSION       = '0.1.21';
 our $COLOUR        = 0;
 our $ABBREV        = 0;
 our $SORT          = 0;
@@ -684,7 +684,7 @@ OptArgs - integrated argument and option processing
 
 =head1 VERSION
 
-0.1.21_1 (yyyy-mm-dd)
+0.1.21 (yyyy-mm-dd)
 
 =head1 SYNOPSIS
 
