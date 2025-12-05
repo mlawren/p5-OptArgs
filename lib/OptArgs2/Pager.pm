44222e6f175df951bb83f196067876775e2106ac
Mark Lawrence <mark@rekudos.net>
2025-12-05 10:24:42 +0100

    Start development on version v0.0.0

--- lib/OptArgs2/Pager.pm
+++ lib/OptArgs2/Pager.pm
@@ -101,7 +101,7 @@ sub pid {
   },
   ;
 
-our $VERSION  = 'v2.0.16';
+our $VERSION  = 'v0.0.0';
 our @CARP_NOT = (__PACKAGE__);
 
 sub _build_pager {
@@ -207,7 +207,7 @@ OptArgs2::Pager - pipe output to a system (text) pager
 
 =head1 VERSION
 
-v2.0.16 (2025-12-05)
+v0.0.0 (yyyy-mm-dd)
 
 =head1 SYNOPSIS
 
